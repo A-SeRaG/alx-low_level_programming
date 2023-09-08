439b75d6e46073c443502e10cac7c5412bb32575
SeRaG <aserag0101@gmail.com>
2023-09-08 06:17:39 -0700

    c programing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d");
+	printf("\n");
+	return (0);
+}
