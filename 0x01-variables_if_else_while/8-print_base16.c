ff6ab41fc742babf67d8357d4d095995ec0f253b
SeRaG <aserag0101@gmail.com>
2023-09-08 07:59:29 -0700

    c programing

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: A C program that prints with putchar function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	char C = 'a';
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		putchar("%d", a);
+	while (C <= 'f')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
