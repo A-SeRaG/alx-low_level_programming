e0df1a807095597dea12b74e0f473a641504b3c0
SeRaG <aserag0101@gmail.com>
2023-09-20 08:11:44 -0700

    c programing

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string from src to dest
+ * @dest: input string
+ * @src: input string
+ * @n: number of elements to copy
+ * Return: string dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+		dest[i] = src[i++];
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
