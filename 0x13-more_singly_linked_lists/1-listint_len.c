9deb09e3fab2cb5b74fc7cc98202706e5331a6b7
SeRaG <aserag0101@gmail.com>
2023-10-23 20:58:29 -0700

    c programing

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint_len - returns the number of elements
+ * listint_len - returns the number of elements
  * @h: pointer to first node
  * Return: size of list
  */
 
-size_t print_listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t n = 0;
 
