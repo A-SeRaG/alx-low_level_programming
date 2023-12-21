6ecdb731f90d1621fa7c647e803a394aeba644b5
SeRaG <aserag0101@gmail.com>
2023-12-21 02:57:09 -0800

    C fun

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,69 @@
+#include "hash_tables.h"
+
+/**
+ * make_node - function make new node
+ * @key: is the key
+ * @value: is the value
+ * Return: new node
+*/
+
+hash_node_t *make_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc()sizeof(hash_node_t);
+	if (node == NULL)
+		return (NULL);
+	node->ket = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL):
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @key: is the key
+ * @value: is the value
+ * Retirn: 1 if it succeeded, 0 otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *hash_node, *temp;
+	char *new_value;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			free(temp->value);
+			temp->value = new_value;
+			return (1);
+		}
+		temp = temp->next;
+	}
+	hash_node = make_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht ->array[index];
+	ht->array[index] = hash_node;
+	return (1);
+}
